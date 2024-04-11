f3e84d1207e510e9d0e494c4843c641e5a94fdce
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-11 15:18:10 -0400

    Time: 1243 ms (6.63%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -2,6 +2,4 @@
 
 with temp as
 (SELECT num, count(num) as unique_num from mynumbers GROUP BY num having count(num) = 1 ORDER BY num DESC LIMIT 1)
-select (CASE WHEN count(num) = 0 then null ELSE num END) as num from temp  -- logic to handle empty table
-
--- SELECT max(num) as num FROM mynumbers where num IN (SELECT num as unique_num from mynumbers GROUP BY num having count(num) = 1) optimal solution 
+select (CASE WHEN count(num) = 0 then null ELSE num END) as num from temp 
