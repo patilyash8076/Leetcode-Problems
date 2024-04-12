05c12a015d75166b45a0276d5a967970b6f84d57
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-11 22:08:46 -0400

    Time: 442 ms (88.82%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
-SELECT x,y,z,
-    CASE WHEN x + y > z and y + z > x  and x + z > y then "Yes" ELSE  "No" END as triangle
+-- SELECT x,y,z,
+--     SUM(CASE WHEN x + y > z then 1
+--      WHEN y + z > x then 1
+--      WHEN x + z > y then 1
+--     ELSE 0) END AS total_len
+-- FROM triangle
+
+SELECT *,
+    CASE WHEN x+y>z and y+z>x and x+z > y then "Yes" ELSE 'No' END AS triangle
 FROM triangle
