bc5f081cf215b1a28f2498046045b6f20faa16dd
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-12 15:35:39 -0400

    Time: 837 ms (90.57%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -10,14 +10,24 @@
 -- From cte where num=num1 and num=num2
 
 -- select * from logs order by id
-SELECT DISTINCT i1.num AS ConsecutiveNums
-FROM 
-    logs i1,
-    logs i2,
-    logs i3
-WHERE
-i1.id = i2.id+1 and
-i2.id = i3.id+1 and
-i1.num = i2.num and
-i2.num = i3.num
 
+-- SELECT DISTINCT i1.num AS ConsecutiveNums
+-- FROM 
+--     logs i1,
+--     logs i2,
+--     logs i3
+-- WHERE
+-- i1.id = i2.id+1 and
+-- i2.id = i3.id+1 and
+-- i1.num = i2.num and
+-- i2.num = i3.num
+
+WITH ConsecutiveLogs AS (
+    SELECT num,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+           LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
+    FROM Logs
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM ConsecutiveLogs
+WHERE num = next_num AND num = next_next_num
