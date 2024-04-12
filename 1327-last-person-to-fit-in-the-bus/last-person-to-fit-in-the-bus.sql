cf683d7c4e6b7cca214f7465b885548fd5bcdf53
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-12 17:08:12 -0400

    Time: 1512 ms (69.96%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,17 +1,19 @@
 # Write your MySQL query statement below
--- with temp as (
--- select PERSON_NAME, weight, turn,
---     sum(weight) OVER(order by turn) as cum_weight
--- FROM Queue
--- ORDER BY turn asc )
-
--- SELECT person_name FROM temp where cum_weight <= 1000 ORDER BY cum_weight desc LIMIT 1
-
-SELECT person_name from (
+with temp as (
 select PERSON_NAME, weight, turn,
     sum(weight) OVER(order by turn) as cum_weight
 FROM Queue
-ORDER BY turn asc ) p1
-WHERE cum_weight <= 1000
-ORDER BY cum_weight desc
-LIMIT 1
+ORDER BY turn asc )
+
+SELECT person_name FROM temp where cum_weight <= 1000 ORDER BY cum_weight desc LIMIT 1
+
+-- LOGIC 2nd
+
+-- SELECT person_name from (
+-- select PERSON_NAME, weight, turn,
+--     sum(weight) OVER(order by turn) as cum_weight
+-- FROM Queue
+-- ORDER BY turn asc ) p1
+-- WHERE cum_weight <= 1000
+-- ORDER BY cum_weight desc
+-- LIMIT 1
