0680e4906c0dbe1597b46d9ac2ec7045ab3004d9
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-12 15:43:44 -0400

    Time: 1081 ms (41.37%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,33 +1,26 @@
 -- # Write your MySQL query statement below
--- with cte as(
-    
---     SELECT num,
---     lead(num,1) OVER(ORDER by ID) as num1,
---     LEAD(num ,2) OVER(ORDER by ID) as num2
---     FROM LOGS
--- )
--- select DISTINCT num as ConsecutiveNums
--- From cte where num=num1 and num=num2
+
 
 -- select * from logs order by id
 
--- SELECT DISTINCT i1.num AS ConsecutiveNums
--- FROM 
---     logs i1,
---     logs i2,
---     logs i3
--- WHERE
--- i1.id = i2.id+1 and
--- i2.id = i3.id+1 and
--- i1.num = i2.num and
--- i2.num = i3.num
+SELECT DISTINCT i1.num AS ConsecutiveNums
+FROM 
+    logs i1,
+    logs i2,
+    logs i3
+WHERE
+i1.id = i2.id+1 and
+i2.id = i3.id+1 and
+i1.num = i2.num and
+i2.num = i3.num
 
-WITH ConsecutiveLogs AS (
-    SELECT num,
-           LEAD(num, 1) OVER (ORDER BY id) AS next_num,
-           LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
-    FROM Logs
-)
-SELECT DISTINCT num AS ConsecutiveNums
-FROM ConsecutiveLogs
-WHERE num = next_num AND num = next_next_num
+-- WITH cte AS (
+--     SELECT num,
+--            LEAD(num, 1) OVER (ORDER BY id) AS num1,
+--            LEAD(num, 2) OVER (ORDER BY id) AS num2
+--     FROM Logs
+-- )
+-- SELECT DISTINCT num AS ConsecutiveNums
+-- FROM cte
+-- WHERE num = num1 AND num1 = num2;
+
