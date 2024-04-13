60577581880570507abcfbc35503aef7c8055100
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-12 20:26:38 -0400

    Time: 1430 ms (96.87%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 with temp as(
-SELECT dpt.name as Department, em.name as Employee,salary,
-DENSE_RANK() OVER(PARTITION BY dpt.name ORDER BY salary desc) as sal_rank
-FROM EMPLOYEE em
-INNER JOIN DEPARTMENT dpt ON em.departmentId  = dpt.id)
+    SELECT dpt.name as Department, em.name as Employee,salary,
+    DENSE_RANK() OVER(PARTITION BY dpt.name ORDER BY salary desc) as sal_rank
+    FROM EMPLOYEE em
+    INNER JOIN DEPARTMENT dpt ON em.departmentId  = dpt.id
+)
 
 select Department, Employee, salary 
 FROM temp
