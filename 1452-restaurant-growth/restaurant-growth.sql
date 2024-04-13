eeb334f57c7dcb64eab73671c62aa3e4ffa2e3f6
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-04-13 01:19:47 -0400

    Time: 613 ms (82.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT VISITED_ON, AMOUNT, ROUND(AMOUNT/7,2) AS  average_amount FROM
+    (SELECT DISTINCT visited_on,
+        SUM(amount) OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) amount, 
+        MIN(visited_on) OVER() 1st_date 
+        FROM Customer) AS TEMP_TABLE
+WHERE VISITED_ON >= 1st_date+6 
+    
