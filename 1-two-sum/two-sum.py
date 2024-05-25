7fe1b02b3152b5fc058451c17dc0db2564eaefc0
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-05-25 12:04:01 -0400

    Time: 61 ms (57.12%) | Memory: 18.5 MB (6.42%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,44 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         my_dict ={}
+#         for i in range(len(nums)):
+#             my_dict[nums[i]] = i 
+#         sorted_nums = sorted(nums)
+#         left = 0
+#         right = len(sorted_nums)-1
+#         while left < right:
+#             current_sum = sorted_nums[left] + sorted_nums[right]
+#             if current_sum > target:
+#                 right = right - 1
+#             elif current_sum < target:
+#                 left = left + 1
+#             else:
+#                 # if sorted_nums[left] == sorted_nums[right]:                    
+#                 #     return [my_dict[sorted_nums[left]][0],my_dict[sorted_nums[right]][1]]
+#                 return [my_dict[sorted_nums[left]],my_dict[sorted_nums[right]]]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Building a dictionary to map values to all their indices
+        num_to_indices = {}
+        for index, num in enumerate(nums):
+            if num not in num_to_indices:
+                num_to_indices[num] = []
+            num_to_indices[num].append(index)
+
+        # Sort the numbers to use two-pointer technique
+        nums.sort()
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            current_sum = nums[left] + nums[right]
+            if current_sum > target:
+                right -= 1
+            elif current_sum < target:
+                left += 1
+            else:  # current_sum == target
+                # If elements are the same, handle accordingly
+                if nums[left] == nums[right]:
+                    return [num_to_indices[nums[left]][0], num_to_indices[nums[right]][1]]
+                return [num_to_indices[nums[left]][0], num_to_indices[nums[right]][0]]
+
+        return []  # If no two numbers sum up to the target
