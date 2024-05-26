7946a0cd85ea0e034dfe7b3630195449e036b384
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-05-26 02:08:19 -0400

    Time: 550 ms (94.59%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
-SELECT
-Row_number() OVER() AS ID, STUDENT
+-- SELECT
+-- Row_number() OVER() AS ID, STUDENT
+-- FROM SEAT
+-- ORDER BY if(mod(id,2) = 0, id-1, id+1)
+
+SELECT 
+CASE WHEN ID = (SELECT MAX(ID) FROM SEAT) and ID % 2 = 1 THEN ID
+     WHEN ID % 2 = 0 THEN ID-1
+     WHEN ID % 2 = 1 THEN  ID+1
+     END AS ID,
+     STUDENT
 FROM SEAT
-ORDER BY if(mod(id,2) = 0, id-1, id+1)
+ORDER BY ID
