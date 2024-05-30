cc17605240d644a5edb55831856b72339791ca88
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-05-30 01:03:22 -0400

    Time: 674 ms (95.78%) | Memory: 26.1 MB (99.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        min_price = prices[0]
+        # O(n) solution 
+        min_price = prices[0]   # assign any number I can also assign infiniy here
         max_profit = 0
         for price in prices:
-            if price < min_price:
-                min_price = price
+            if price < min_price:   # checks if price is less than min_price
+                min_price = price   # if it is then assigning min_price with current_price
             else:
-                diff = price - min_price
-                if diff > max_profit:
+                diff = price - min_price    # if not then difference is calculates between current_price and min_price 
+                if diff > max_profit:       # if diff is greater than max_profit then diff is assign to max_profit
                     max_profit = diff
         return max_profit
+
+        # O(n**2) Solution
+
         # max_profit = 0
         # for i in range(len(prices)-1):          #[7 1 5 3 6]
         #     for j in range(i+1, len(prices)):   #[1 5 3 6 4]
