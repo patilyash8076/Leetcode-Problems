1bc1d369ce4823c36ae177dc166ac9c696fe0ea6
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-01 02:53:42 -0400

    Time: 40 ms (81.15%) | Memory: 17.2 MB (47.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str_lower = s.lower()
-        string = str_lower
-        if len(str_lower) == 1:
+        string = s.lower()
+        if len(string) == 1:
             return True
         for char in "\"!#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
             string = string.replace(char, '')
         string = string.replace(' ', '')
-        print(string)
         if string == string[::-1]:
             return True
         return False
