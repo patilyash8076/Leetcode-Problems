e3e80bacc9e1ffe8c5a0df14afcac010aecd1529
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-04 03:10:08 -0400

    Time: 37 ms (54.97%) | Memory: 16.6 MB (80.97%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -6,32 +6,29 @@ class Solution:
         summ = 0
         s_lower = s
         count_dict = Counter(s_lower)
-        # print(count_dict)
         occur_list = count_dict.values()
         occurences = sorted(occur_list, reverse = True)
+        # created two different list one to store odd numbers and another to store even numbers
         for i in occurences:
             if i % 2 == 0:
                 even_list.append(i)
             elif i % 2 != 0:
                 odd_list.append(i)
+        # check if odd_list have element more than 1 then appending 1st element i.e biggest odd number 
+        # to even list
         if len(odd_list) > 0:
             even_list.append(odd_list[0])
-        print(odd_list)
-        print(even_list)
-        if len(odd_list) > 1:
             for i in range(1, len(odd_list)):
                 if odd_list[i] != 1:
                     odd_list[i] -= 1
                     even_list.append(odd_list[i])
+
         if len(even_list) == 1:
             return even_list[0]
         else:
             print('even_list: ',even_list)
             for i in range(0,len(even_list)):
-                # if even_list[i] % 2 == 0 or even_list[i] == 1:
                 summ += even_list[i]
-                # elif even_list[i] % 2 != 0:
-                #     summ += even_list[i] - 1
             return summ
 
 
