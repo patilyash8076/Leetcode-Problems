df4d52fd09036c9d32bf78eb6c2d69a17cfbfae4
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-05 10:53:44 -0400

    Time: 50 ms (53.96%) | Memory: 16.6 MB (67.21%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -6,8 +6,8 @@ class Solution:
         for i in range(1,len(words)):
             current_count = Counter(words[i])
             for letter in common_count.keys():#['l', 'b', 'e', 'a']
-                common_count[letter] = min(current_count[letter], common_count[letter])
-            # print(common_count)
+                common_count[letter] = min(current_count[letter], common_count[letter]) # common_count['l'] = min (1,2)
+
         for letter, count in common_count.items():
             for _ in range(count):
                 result_list.append(letter)
