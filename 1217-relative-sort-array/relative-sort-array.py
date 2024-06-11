0e1b7d29c415f8d5fcb5d9dcc87ce54e33f80bd4
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-10 22:03:58 -0400

    Time: 47 ms (22.92%) | Memory: 16.7 MB (67.06%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr1_dict = {}
+        arr2_dict = {}
+        li = []
+        sort_list =[]
+        li2 = []
+        for i, num in enumerate(arr2):
+            arr2_dict[num] = i
+        for num, i in enumerate(arr1):
+            if i in arr1_dict:
+                arr1_dict[i].append(num)
+            else:
+                arr1_dict[i] = [num]
+        for i in arr2_dict:
+            for count in range(len(arr1_dict[i])):
+                li.append(i)
+        for i in arr1:
+            if i not in arr2:
+                li2.append(i)
+        sort_list = sorted(li2)
+        for i in sort_list:
+            li.append(i)
+        return li
