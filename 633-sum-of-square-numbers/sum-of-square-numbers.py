21af8461c648d1082ed8bfcdb54b4bc88ec47823
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-17 19:15:11 -0400

    Time: 193 ms (10.48%) | Memory: 18.2 MB (9.52%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,33 +1,17 @@
-# class Solution:
-#     def judgeSquareSum(self, c: int) -> bool:
-#         sq_list = [i for i in range(0,c+1)]
-#         left = 0
-#         right = int(c**0.5)
-#         while left <= right:
-#             sum_of_vars = sq_list[left]**2  + sq_list[right]**2
-#             left_sum = sq_list[left]**2 + sq_list[left]**2
-#             right_sum = sq_list[right]**2 + sq_list[right]**2
-#             if sum_of_vars == c or left_sum == c or right_sum == c:
-#                 return  True
-#             elif sum_of_vars > c:
-#                 right -= 1
-#             elif sum_of_vars < c:
-#                 left += 1
-#         return False
-
+from math import sqrt
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
+        sq_list = [i for i in range(int(math.sqrt(c)) + 1)]
         left = 0
-        right = int(c**0.5)  # Only need to consider up to the square root of c
+        right = int(c**0.5)
         while left <= right:
-            current_sum = left**2 + right**2
-            if current_sum == c:
-                return True
-            elif current_sum > c:
+            sum_of_vars = sq_list[left]**2  + sq_list[right]**2
+            left_sum = sq_list[left]**2 + sq_list[left]**2
+            right_sum = sq_list[right]**2 + sq_list[right]**2
+            if sum_of_vars == c or left_sum == c or right_sum == c:
+                return  True
+            elif sum_of_vars > c:
                 right -= 1
-            else:
+            elif sum_of_vars < c:
                 left += 1
         return False
-
-            
-        
