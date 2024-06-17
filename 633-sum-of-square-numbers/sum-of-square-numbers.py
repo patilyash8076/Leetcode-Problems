389a3ffc438fd47b6d4d75f76ca12f76930ea62c
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-06-17 19:15:37 -0400

    Time: 105 ms (43.32%) | Memory: 18 MB (10.70%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -6,9 +6,9 @@ class Solution:
         right = int(c**0.5)
         while left <= right:
             sum_of_vars = sq_list[left]**2  + sq_list[right]**2
-            left_sum = sq_list[left]**2 + sq_list[left]**2
-            right_sum = sq_list[right]**2 + sq_list[right]**2
-            if sum_of_vars == c or left_sum == c or right_sum == c:
+            # left_sum = sq_list[left]**2 + sq_list[left]**2
+            # right_sum = sq_list[right]**2 + sq_list[right]**2
+            if sum_of_vars == c: #or left_sum == c or right_sum == c:
                 return  True
             elif sum_of_vars > c:
                 right -= 1
