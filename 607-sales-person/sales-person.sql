9763be1d494ba2e5e817b9d0f78afd4a8917651a
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-08-08 10:49:20 -0400

    Time: 1616 ms (16.09%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-with sales_cte as(
-Select sales_id from Orders where com_id = (select com_id from company where name = 'RED'))
+-- with sales_cte as(
+-- Select sales_id from Orders where com_id = (select com_id from company where name = 'RED'))
+-- select name from SalesPerson where sales_id not in (select sales_id from sales_cte)
 
-select name from SalesPerson where sales_id not in (select sales_id from sales_cte)
--- select * from sales_cte
+SELECT name FROM SalesPerson 
+WHERE sales_id NOT IN
+     (SELECT sales_id FROM Orders 
+     WHERE com_id = (SELECT com_id FROM company WHERE name = 'RED'))
