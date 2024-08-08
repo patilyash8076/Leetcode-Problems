46dcf2785582d2c62dbc652adcb877d462b6a99d
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-08-08 11:04:53 -0400

    Time: 432 ms (92.06%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,4 +1,14 @@
 # Write your MySQL query statement below
-SELECT player_id, min(event_date) as first_login
-FROM Activity
-GROUP BY player_id
+-- SELECT player_id, min(event_date) as first_login
+-- FROM Activity
+-- GROUP BY player_id
+
+WITH temp_cte as(
+SELECT player_id,
+DENSE_RANK() OVER(Partition by player_id ORDER BY event_date) as rankk,
+event_date
+FROM Activity)
+
+SELECT player_id, event_date as first_login
+FROM temp_cte
+WHERE rankk = 1
