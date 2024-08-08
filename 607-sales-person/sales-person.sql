50f4c5a226555a5e353164f9d4f01c4d6e10b8d0
Yash Madhukar Patil <52972983+patilyash8076@users.noreply.github.com>
2024-08-08 10:51:56 -0400

    Time: 1268 ms (63.72%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -3,7 +3,9 @@
 -- Select sales_id from Orders where com_id = (select com_id from company where name = 'RED'))
 -- select name from SalesPerson where sales_id not in (select sales_id from sales_cte)
 
-SELECT name FROM SalesPerson 
-WHERE sales_id NOT IN
-     (SELECT sales_id FROM Orders 
+WITH sales_cte as
+(SELECT sales_id FROM Orders 
      WHERE com_id = (SELECT com_id FROM company WHERE name = 'RED'))
+
+SELECT name FROM SalesPerson  
+WHERE sales_id NOT IN (SELECT * FROM sales_cte)
